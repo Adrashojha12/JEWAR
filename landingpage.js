7933567ff1d82b1995fa981cdbee6b070a454ac8
Adrashojha12 <120448373+Adrashojha12@users.noreply.github.com>
2023-01-07 11:16:37 +0530

    Add files via upload

---
+++ landingpage.js
@@ -0,0 +1,33 @@
+$('.carousel').carousel({
+    interval: 6000,
+    pause: "false"
+  });
+  
+$(document).ready(function() {
+    $("#news-slider").owlCarousel({
+        items : 3,
+        itemsDesktop:[1199,3],
+        itemsDesktopSmall:[980,2],
+        itemsMobile : [600,1],
+        navigation:true,
+        navigationText:["",""],
+        pagination:true,
+        autoPlay:true
+    });
+});
+
+
+function prev(){
+    document.getElementById('slider-container').scrollLeft -= 270;
+}
+
+function next()
+{
+    document.getElementById('slider-container').scrollLeft += 270;
+}
+
+
+$(".slide img").on("click" , function(){
+$(this).toggleClass('zoomed');
+$(".overlay").toggleClass('active');
+})
